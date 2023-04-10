73e8418476d9fcfdb5485b59642395c5fe80f84b
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-04-10 12:46:47 +0200

    integrate with prisma shopdb

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -18,20 +18,20 @@ export class ProductsController {
   constructor(private productsService: ProductsService) {}
 
   @Get('/')
-  getAll(): any {
+  async getAll() {
     return this.productsService.getAll();
   }
 
   @Get('/:id')
-  getById(@Param('id', new ParseUUIDPipe()) id: string) {
-    const prod = this.productsService.getById(id);
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const prod = await this.productsService.getById(id);
     if (!prod) throw new NotFoundException('Product not found');
     return prod;
   }
 
   @Delete('/:id')
-  public deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
-    if (!this.productsService.getById(id))
+  async deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
+    if (!(await this.productsService.getById(id)))
       throw new NotFoundException('Product not found');
     this.productsService.deleteById(id);
     return { success: true };
@@ -43,11 +43,11 @@ export class ProductsController {
   }
 
   @Put('/:id')
-  update(
+  async update(
     @Param('id', new ParseUUIDPipe()) id: string,
     @Body() productData: UpdateProductDTO,
   ) {
-    if (!this.productsService.getById(id))
+    if (!(await this.productsService.getById(id)))
       throw new NotFoundException('Product not found');
     this.productsService.updateById(id, productData);
     return { success: true };
